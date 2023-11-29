dd1d37c4a8bcbd12b8c94ed61259c1cf9cb535a4
Alcemir Macedo <alcemirmacedo@gmail.com>
2023-11-29 07:48:59 -0400

    inicial sacale on metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, inicial-scale=1.0" />
       <body className={inter.className}>{children}</body>
     </html>
   )
